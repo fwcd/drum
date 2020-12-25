1ae01b71720e9c0fd3db092f9f33eebb3f03d9ae
fwcd <fwcdmail@gmail.com>
2020-12-25 16:53:23 +0100

    Specify nullabilities in schema explicitly

--- lib/drum/db.rb
+++ lib/drum/db.rb
@@ -11,57 +11,57 @@ module Drum
     db.create_table?(:tracks) do
       primary_key :id
       # Generic metadata
-      String :title
-      Integer :duration_ms
-      TrueClass :explicit
-      String :isrc
+      String :title, null: false
+      Integer :duration_ms, null: false
+      TrueClass :explicit, null: true
+      String :isrc, null: true
       # General audio features
-      Float :tempo            # beats per minute
-      Integer :key            # 0 = C, 1 = C#/D, ...
-      Integer :mode           # 0 = minor, 1 = major
-      Integer :time_signature # beats per measure
+      Float :tempo, null: true            # beats per minute
+      Integer :key, null: true            # 0 = C, 1 = C#/D, ...
+      Integer :mode, null: true           # 0 = minor, 1 = major
+      Integer :time_signature, null: true # beats per measure
       # Specific audio features
-      Float :acousticness     # 0.0 to 1.0, 1.0 = high confidence
-      Float :danceability     # 0.0 to 1.0, 1.0 = very danceable
-      Float :energy           # 0.0 to 1.0, 1.0 = high energy
-      Float :instrumentalness # 0.0 to 1.0, > 0.5 = instrumentals
-      Float :liveness         # 0.0 to 1.0, > 0.8 = live audience very likely
-      Float :loudness         # dB
-      Float :speechiness      # 0.0 to 1.0, > 0.6 = only spoken words
-      Float :valence          # 0.0 to 1.0, musical positiveness, higher = happier
+      Float :acousticness, null: true     # 0.0 to 1.0, 1.0 = high confidence
+      Float :danceability, null: true     # 0.0 to 1.0, 1.0 = very danceable
+      Float :energy, null: true           # 0.0 to 1.0, 1.0 = high energy
+      Float :instrumentalness, null: true # 0.0 to 1.0, > 0.5 = instrumentals
+      Float :liveness, null: true         # 0.0 to 1.0, > 0.8 = live audience very likely
+      Float :loudness, null: true         # dB
+      Float :speechiness, null: true      # 0.0 to 1.0, > 0.6 = only spoken words
+      Float :valence, null: true          # 0.0 to 1.0, musical positiveness, higher = happier
     end
     
     db.create_table?(:artists) do
       primary_key :id
-      String :name
+      String :name, null: false
     end
     
     db.create_table?(:track_artists) do
-      foreign_key :artist_id, :artists
-      foreign_key :track_id, :tracks
+      foreign_key :artist_id, :artists, null: false
+      foreign_key :track_id, :tracks, null: false
       primary_key [:artist_id, :track_id]
     end
     
     db.create_table?(:users) do
       primary_key :id
-      String :name
-      String :display_name
+      String :name, null: false
+      String :display_name, null: true
     end
 
     db.create_table?(:playlists) do
       primary_key :id
-      String :name
-      String :description
+      String :name, null: false
+      String :description, null: true
       foreign_key :user_id, :users
     end
     
     db.create_table?(:playlist_tracks) do
-      foreign_key :playlist_id, :playlists
-      foreign_key :track_id, :tracks
+      foreign_key :playlist_id, :playlists, null: false
+      foreign_key :track_id, :tracks, null: false
       primary_key [:playlist_id, :track_id]
-      Integer :track_index # within the playlist
-      DateTime :added_at
-      foreign_key :user_id, :users
+      Integer :track_index, null: true # within the playlist, null if unordered
+      DateTime :added_at, null: true
+      foreign_key :user_id, :users, null: false
     end
 
     return db
