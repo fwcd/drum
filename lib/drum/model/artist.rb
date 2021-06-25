bb76ab59f0006d0dae3f24d318277bf376510a2c
fwcd <fwcdmail@gmail.com>
2021-06-25 02:01:37 +0200

    Add deserialize methods to artist

--- lib/drum/model/artist.rb
+++ lib/drum/model/artist.rb
@@ -11,7 +11,19 @@ module Drum
     :id,
     :name,
     :spotify
-  )
+  ) do
+    # Parses an artist from a nested Hash that uses string keys.
+    #
+    # @param [Hash<String, Object>] h The Hash to be parsed
+    # @return [Artist] The parsed artist
+    def self.deserialize(h)
+      Artist.new(
+        id: h['id'],
+        name: h['name'],
+        spotify: h['spotify'].try { |s| ArtistSpotify.deserialize(s) }
+      )
+    end
+  end
 
   # Spotify-specific metadata about the artist.
   #
@@ -19,5 +31,15 @@ module Drum
   #   @return [String] The id of the artist on Spotify
   ArtistSpotify = Struct.new(
     :id
-  )
+  ) do
+    # Parses spotify metadata from a Hash that uses string keys.
+    #
+    # @param [Hash<String, Object>] h The Hash to be parsed
+    # @return [ArtistSpotify] The parsed metadata
+    def self.deserialize(h)
+      AlbumSpotify.new(
+        id: h['id']
+      )
+    end
+  end
 end
